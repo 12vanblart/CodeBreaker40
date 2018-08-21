99c77e3ddfe3284bd1aad0502ec4d6a45fe061d8
12vanblart <12vanblart@gmail.com>
2018-08-20 20:47:57 -0400

    Added roate.py for calling the rotation function

---
+++ Rewrite - Python 3/rotate.py
@@ -0,0 +1,12 @@
+##############################
+##rotate.py ##################
+##Python 3 ###################
+##############################
+
+###################################################################################################
+#This will contain everything needed to rotate the puzzle                                         #
+###################################################################################################
+
+##Pass in the following variables
+##### Turns Made - Number of turns made thus far. This will start as 0, then increment each time this module is called. 
+##### Puzzle - The puzzle in it's current state. This will be returned as the updated puzzle state
