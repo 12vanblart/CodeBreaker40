7fa600f35de67b878f512b2378e60ed0bc9cc84f
12vanblart <12vanblart@gmail.com>
2018-08-22 18:56:22 -0400

    Corrected checkSolution.py to consider cogs (default to X)

--- Rewrite - Python 3/TestCheckSolution.py
+++ Rewrite - Python 3/TestCheckSolution.py
@@ -12,10 +12,13 @@ def main():
   successful = 0
   totalTests = 0
   print("Running Test 1")
-  puzzle1 = [[1,2,5],[3,2,0],[1,1,0]]
-  puzzle1Solution = 5
+  puzzle1 = [[1,2,3],[3,2,0],[2,1,0]]
+  puzzle1Solution = 3
   puzzle2 = [[1,1,1],[4,3,5],[0,1,5]]
   puzzle2Solution = 40
+  puzzle3 = [[2,1,1],[0,5,3],[4,"X",5]]
+  puzzle3Solution = 6
+  ##Test 1
   result1 = checkSolution(puzzle1, puzzle1Solution)
   totalTests+= 1
   if (result1 == True):
@@ -23,6 +26,7 @@ def main():
     print("Test 1 PASSED")
   else: 
     print("Test 1 FAILED")
+  ##Test 2
   result2 = checkSolution(puzzle2, puzzle2Solution)
   totalTests+= 1
   if (result2 == False ):
@@ -30,6 +34,15 @@ def main():
     print("Test 2 PASSED")
   else:
     print("Test 2 FAILED")
+  ##Test 3
+  result3 = checkSolution(puzzle3, puzzle3Solution)
+  totalTests+= 1
+  if (result1 == True):
+    successful+= 1
+    print("Test 3 PASSED")
+  else: 
+    print("Test 3 FAILED")
+  
   #Print total successes count
   print("Tests passed: ", successful, "/", totalTests)
   
--- Rewrite - Python 3/checkSolution.py
+++ Rewrite - Python 3/checkSolution.py
@@ -13,17 +13,22 @@
 ##### FALSE if not a solution
 ##### TRUE if it is a solution
 
-##Check Solution for a puzzle with no "cog" pieces covering lower layers
-def checkSolution(puzzle, value):
+##Check Solution for a puzzle with "cog" pieces covering lower layers
+def checkSolution(puzzle, value, cog = "X"): 
+  ## `cog` is the icon denoting to use the below number
   response = True
   for col in range(len(puzzle[0])):
     total = 0
-    for row in range(len(puzzle)):
-      total += puzzle[row][col]
+    rowVal = 0
+    while (rowVal <= len(puzzle)):
+      if puzzle[rowVal][col] == cog:
+        total += puzzle[rowVal-1][col]
+      else:
+        total += puzzle[rowVal][col]
+      rowVal += 2
     if total != value:
       response = False
       break
   return response
-
-##Check Solution for a puzzle with "cog" pieces covering lower layers
-
+##NOTE: IF this returns TRUE, need to store solution to ensure uniqueness##
+  
