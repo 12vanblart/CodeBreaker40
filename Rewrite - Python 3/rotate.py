ad5f166ced7452bd2c81ca3c7568fb342fb39f34
12vanblart <12vanblart@gmail.com>
2018-08-23 07:24:49 -0400

    Starting Rotation work

--- Rewrite - Python 3/rotate.py
+++ Rewrite - Python 3/rotate.py
@@ -10,3 +10,13 @@
 ##Pass in the following variables
 ##### Turns Made - Number of turns made thus far. This will start as 0, then increment each time this module is called. 
 ##### Puzzle - The puzzle in it's current state. This will be returned as the updated puzzle state
+
+def rotate(puzzleIn, turnsMade):
+  #First, rotate the first (2) rows [Base Layer]#
+  turnableUnits = int((len(puzzleIn)-1)/2)# for SC 40, this should return 3#
+  for unit in range(turnableUnits): # For each turnable unit...
+    print(unit)
+    
+    
+if __name__ == "__main__":
+  rotate([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]], 0)
