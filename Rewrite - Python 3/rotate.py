a167c857292689e930d6a57c74526cd5ba432ac8
12vanblart <12vanblart@gmail.com>
2018-08-26 11:29:01 -0400

    added notes for rotate.py

--- Rewrite - Python 3/rotate.py
+++ Rewrite - Python 3/rotate.py
@@ -10,3 +10,11 @@
 ##Pass in the following variables
 ##### Turns Made - Number of turns made thus far. This will start as 0, then increment each time this module is called. 
 ##### Puzzle - The puzzle in it's current state. This will be returned as the updated puzzle state
+def rotate(puzzleIn, turnsMade):
+  
+  
+##  >>> lst = ['string1', 'string2', 'string3']
+##  >>> lst.append(lst.pop(lst.index('string2')))
+##Use above to pop the first item in an array to the end of the array.  
+  
+  
