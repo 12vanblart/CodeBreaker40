b8ce1549fcb9316d8a810defa1893aa19143775d
12vanblart <12vanblart@gmail.com>
2018-08-31 07:27:52 -0400

    Added logic for Cogs to Increment Array

--- Rewrite - Python 3/incrementArray.py
+++ Rewrite - Python 3/incrementArray.py
@@ -13,15 +13,19 @@ def incrementArray(arrIn, maxVal):
   arrIn[0][0] = arrIn[0][0] + 1
   for row in range(len(arrIn)):
     for col in range(len(arrIn[row])):
-      if arrIn[row][col] == maxVal:
-        arrIn[row][col] = 0
-        if col == len(arrIn[row])-1:
-          if row != len(arrIn)-1:
-            #INCREMENT NEXT ROW[0]
-            arrIn[row+1][0] = arrIn[row+1][0] + 1
+      if isinstance(arrIn[row][col], int):
+        if arrIn[row][col] == maxVal:
+          arrIn[row][col] = 0
+          if col == len(arrIn[row])-1:
+            if row != len(arrIn)-1:
+              #INCREMENT NEXT ROW[0]
+              arrIn[row+1][0] = arrIn[row+1][0] + 1
+          else:
+            #INCREMENT NEXT COLUMN'S VALUE
+            if isinstance(arrIn[row][col+1], int):
+              arrIn[row][col+1] = arrIn[row][col+1] + 1
+            else:
+              arrIn[row][col+2] = arrIn[row][col+2] + 1
         else:
-          #INCREMENT NEXT COLUMN'S VALUE
-          arrIn[row][col+1] = arrIn[row][col+1] + 1
-      else:
-        break
+          break
   return arrIn
