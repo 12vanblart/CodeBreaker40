bb4736ab9a86c49a3775e9725892f0c6b289f52c
12vanblart <12vanblart@gmail.com>
2018-08-30 20:54:07 -0400

    Added initial function for countUnique.py

--- Rewrite - Python 3/countUnique.py
+++ Rewrite - Python 3/countUnique.py
@@ -9,4 +9,8 @@
 #The number of columns/rows needs to be dynamic so that any size puzzle can be checked            #
 ###################################################################################################
 
-
+#Collect 2 puzzles in#
+def checkUnique(solution1, solution2):
+  ## Convert Columns -> Strings Array
+  ## Solution 1
+  
