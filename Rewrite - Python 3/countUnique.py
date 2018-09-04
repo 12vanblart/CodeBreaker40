056745791fd00a5134929f379e1fab60ecca4da5
12vanblart <12vanblart@gmail.com>
2018-09-04 11:28:54 +0000

    Finished ver 1 of Unique Count. Added Testing

--- Rewrite - Python 3/countUnique.py
+++ Rewrite - Python 3/countUnique.py
@@ -10,18 +10,44 @@
 ###################################################################################################
 
 #Collect 2 puzzles in#
-def checkUnique(solution1, solution2):
+def checkUnique(solution1, solution2, cog = "X"):
   ## Convert Columns -> Strings Array
   ## Solution 1
+  unique = False # Assuming solutions are not unique by default
   solutionList1 = []
   solutionList2 = []
   for col in range(len(solution1[0])):
     solString = ""
-    for row in range(len(solution1)):
-      solString = solString + str(solution1[row][col])
+    rowVal = 0
+    while (rowVal <= len(solution1)):
+      if solution1[rowVal][col] == cog:
+        solString = solString + str(solution1[rowVal-1][col])
+      else:
+        solString = solString + str(solution1[rowVal][col])
+      rowVal += 2
     solutionList1.append(solString)
-  print(solutionList1)
 
+  ## Solution 2
+  for col in range(len(solution2[0])):
+    solString = ""
+    rowVal = 0
+    while (rowVal <= len(solution2)):
+      if solution2[rowVal][col] == cog:
+        solString = solString + str(solution2[rowVal-1][col])
+      else:
+        solString = solString + str(solution2[rowVal][col])
+      rowVal += 2
+    solutionList2.append(solString)
+  
+  ##Check if Unique
+  while len(solutionList1) > 0:
+    if solutionList1[0] in solutionList2:
+      solutionList1.pop(0)
+    else: 
+      unique = True # Once something doesn't match, they must be unique
+      break;
+  return unique
 
 if __name__ == "__main__":
-  checkUnique([[1,4,7],[2,5,8],[3,6,9]], [[1,1,1],[2,2,2],[3,3,3]])
+  checkUnique([[1,4,7],[2,5,8],[3,"X",9]], [[1,1,1],[2,2,2],[3,"X",3]])
+  checkUnique([[1,1,1],[2,2,2],[3,3,3]], [[1,1,1],[2,2,2],[3,3,3]])
